2612628e6c3f23ad9795e6447b8969bb96f4a765
nikgeorg <georgiou.nick@outlook.com>
2023-04-15 13:46:51 +0300

    cubes printed properly (hopefully....)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,12 @@
 package org.example;
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
         int K;
         Scanner input = new Scanner(System.in);
-        Random rand = new Random();
-        Set<Integer> set = new LinkedHashSet<>();
         System.out.println("Enter an integer: ");
         K = input.nextInt();
 
@@ -17,31 +17,53 @@ public class Main {
         Table table = new Table();
         table.setNumRows(3);
         table.setNumColumns(L);
-        while (set.size() <= N) {
-            set.add(rand.nextInt(1, N));
-        }
+
+        // adding the ids of the cubes here
+        List<Integer> ids = IntStream.rangeClosed(1, N)
+                .boxed().collect(Collectors.toList());
+        Collections.shuffle(ids);
+        int counter = 0;
         for (int r = 1; r <= 3; r++) {
             for (int c = 1; c <= K; c++) {
-                for (Integer i : set) {
-                    table.cubes.add(new Cube(i, c, r));
-                    break;
+                if (counter == N) break;
+                Integer id = ids.get(counter);
+                table.cubes.add(new Cube(id, c, r));
+                counter++;
                 }
+            if (counter == N) break;
             }
-        }
 
-        // printCubes(table);
+        printCubes(table, K);
     }
 
-    public static void printCubes(Table table) {
+    public static void printCubes(Table table, int K) {
         for (int r = 3; r >= 1; r--) {
-            for (int col = 1; col <= table.getNumColumns(); col++) {
-                for (Cube c : table.cubes) {
-                    if (c.getxCoordinate() == col && c.getyCoordinate() == r) {
-                        System.out.print("[" + c.getId() + "]");
-                    }
+            // add a line break after each row
+            if (r != 1) {
+                for (int col = 1; col <= K; col++) {
+                    isCubeFound(table, r, col);
+                }
+            } else {
+                for (int col = 1; col <= table.getNumColumns(); col++) {
+                    isCubeFound(table, r, col);
                 }
             }
-            System.out.print("\n");
+            System.out.print("\n"); // add a line break after each row
         }
     }
+
+    private static void isCubeFound(Table table, int r, int col) {
+        boolean cubeFound = false;
+        for (Cube c : table.cubes) {
+            if (c.getxCoordinate() == col && c.getyCoordinate() == r) {
+                System.out.print("[" + c.getId() + "] ");
+                cubeFound = true;
+                break;
+            }
+        }
+        if (!cubeFound) {
+            System.out.print("   ");
+        }
+    }
+
 }
